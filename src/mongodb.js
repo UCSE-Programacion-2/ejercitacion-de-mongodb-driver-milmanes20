96a4d7964f08fb6b55640e84abbaf34ba029d34c
Marco Castillo Cabrera <marco.cast.cab@gmail.com>
2026-06-22 23:41:08 -0300

    endpoint buscar tecnico

--- src/mongodb.js
+++ src/mongodb.js
@@ -4,6 +4,7 @@ const { MongoClient } = require('mongodb');
 const dns = require('dns');
 dns.setServers(['8.8.8.8']);
 
+
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017';
 
 // Cliente de MongoDB
@@ -14,9 +15,12 @@ const client = new MongoClient(MONGO_URI);
  * 1. Utiliza client.connect() para conectarte al motor de base de datos.
  * Opcional: Imprime por consola que te has conectado correctamente.
  */
+
 async function connectDB() {
     try {
         // Tu código aquí
+        await client.connect();
+        console.log("Conectado correctamente a MongoDB Atlas");
         
     } catch (error) {
         console.error("Error al conectar a MongoDB:", error);
